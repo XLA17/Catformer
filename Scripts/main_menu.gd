adb69abf2cb69e2caf80bb51f52ebee397fa2572
Regoma <quentinbosia@yahoo.fr>
2025-12-07 19:37:52 +0100

    MainMenu

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var level = preload("res://Scenes/main.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_quit_button_down() -> void:
+	get_tree().quit()
+
+
+func _on_start_button_down() -> void:
+	get_tree().change_scene_to_packed(level)
