da04e1c810548972fd62c057db8ca41ad97e2dac
Alexy Lafosse <xla.lafosse@gmail.com>
2025-12-13 19:08:01 +0100

    update: transition

--- Scripts/transition.gd
+++ Scripts/transition.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 signal anim_transition_finished
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "Level_Fade_In":
+	if anim_name == "New_Level_Fade" or anim_name == "Next_Level_Fade":
 		emit_signal("anim_transition_finished")
-	pass # Replace with function body.
+		#await get_tree().create_timer(1.0).timeout
+		#$AnimationPlayer.play("Level_Fade_Out")
+	#elif anim_name == "Level_Fade_Out":
