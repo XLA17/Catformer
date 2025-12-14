8c17da027bb99536067af50cecda7166b11025a1
Regoma <quentinbosia@yahoo.fr>
2025-12-14 19:40:28 +0100

    add: sound volume

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -10,3 +10,7 @@ func _on_quit_pressed() -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(level)
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
